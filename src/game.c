c5b9dac4a6999ae668e3b8c3f18f50f656adceae
Vallium <aalliot@student.42.fr>
2015-01-27 18:31:41 +0100

    joystick no invert

--- src/game.c
+++ src/game.c
@@ -514,11 +514,11 @@ int		game_event_handler(t_game *game)
 	{
 		double test;
 		if (event.jaxis.axis == 1 && (event.jaxis.value > 5000 || event.jaxis.value < -5000))
-			game->input[MOV_Y] = event.jaxis.value;
+			game->input[MOV_Y] = -event.jaxis.value - 1;
 		else if (event.jaxis.axis == 1)
 			game->input[MOV_Y] = 0, Mix_FadeOutChannel(1, 200);
 		if (event.jaxis.axis == 0 && (event.jaxis.value > 5000 || event.jaxis.value < -5000))
-			game->input[MOV_X] = event.jaxis.value;
+			game->input[MOV_X] = -event.jaxis.value - 1;
 		else if (event.jaxis.axis == 0)
 			game->input[MOV_X] = 0, Mix_FadeOutChannel(1, 200);
 		if (event.jaxis.axis == 3 && (event.jaxis.value > 5000 || event.jaxis.value < -5000))
