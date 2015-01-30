4788e95973c35b4e1b607541437624d6607a0756
spectrenoir06 <antoine@doussaud.org>
2015-01-30 19:29:54 +0100

    warning + free

--- src/map.c
+++ src/map.c
@@ -41,10 +41,9 @@ void	ft_kebab(char * buff, const char * first, ...)
 	buff[i] = 0;
 }
 
-void	sprite_load(t_game *game, t_map *map)
+void	sprite_load(t_map *map)
 {
 	int		i;
-	int		j;
 	char	buff[256];
 	char	*nb;
 
@@ -55,7 +54,7 @@ void	sprite_load(t_game *game, t_map *map)
 		ft_kebab(buff, "modes/1/maps/1/sprites/", nb, ".bmp", NULL);
 		map->sprite[i].tex = SDL_LoadBMP(buff);
 		map->sprite[i].frames = map->sprite[i].tex->w / TEX_SIZE;
-		//free(nb);
+		free(nb);
 		i++;
 	}
 }
@@ -63,64 +62,49 @@ void	sprite_load(t_game *game, t_map *map)
 void	map_init(t_game *game)
 {
 	int		x;
-	int		y;
 
+	map_load(&(game->map), "modes/1/maps/1/");
+	sprite_load(&(game->map));
+	weapon_load(game, &(game->map), 1);
 	x = 0;
-	y = 0;
-
-	map_load(game, &(game->map), "modes/1/maps/1/map.bin");
-	sprite_load(game, &(game->map));
-
-	x = 0;
-
 	while (x < 10)
-	{
 		game->input[x++] = 0;
-	}
-
-	x = 0;
-
-	while (x < game->map.nb_entity)
-	{
-		game->map.entity_ptr[x] = &game->map.entity[x];
-		x++;
-	}
 }
 
-int		map_load(t_game *game, t_map *map, char *path)
+int		map_load(t_map *map, char *path)
 {
 	int		i;
 	int		fd;
-	Uint32	textures;
-	int		ret;
+	int		textures;
 	char	buff[256];
+	char	*nb;
 
-	if ((fd = open(path, O_RDONLY)) == -1)
+	ft_kebab(buff, path, "map.bin", NULL);
+	if ((fd = open(buff, O_RDONLY)) == -1)
 		return (-1);
-	if ((ret = read(fd, &(map->lx), 4)) <= 0)
-		return (ret);
-	if ((ret = read(fd, &(map->ly), 4)) <= 0)
-		return (ret);
-	if ((ret = read(fd, &(map->nb_entity), 4)) <= 0)
-			return (ret);
-	if ((ret = read(fd, &textures, 4)) <= 0)
-		return (ret);
+	read(fd, &(map->lx), 4);
+	read(fd, &(map->ly), 4);
+	read(fd, &(map->nb_entity), 4);
+	read(fd, &textures, 4);
+
 	i = 0;
 
+	map->textures = (SDL_Surface **)malloc(sizeof(SDL_Surface *) * textures);
 	while (i < textures)
 	{
-		ft_kebab(buff, "modes/1/maps/1/textures/", ft_itoa(i), ".bmp", NULL);
+		nb = ft_itoa(i);
+		ft_kebab(buff, path, "textures/", nb, ".bmp", NULL);
 		map->textures[i] = SDL_LoadBMP(buff);
+		free(nb);
 		i++;
 	}
-	map->sky = SDL_LoadBMP("modes/1/maps/1/textures/sky.bmp");
+	ft_kebab(buff, path, "textures/sky.bmp", NULL);
+	map->sky = SDL_LoadBMP(buff);
 
-	map->ceil = (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
-	map->wall = (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
-	map->floor = (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
+	map->ceil	= (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
+	map->wall	= (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
+	map->floor	= (Uint8 *)ft_malloc(sizeof(Uint8) * map->lx * map->ly);
 
-	map->entity = (t_entity *)ft_malloc(sizeof(t_entity) * map->nb_entity);
-	map->entity_ptr = (t_entity **)ft_malloc(sizeof(t_entity * ) * map->nb_entity);
 	printf("entity number = %d\n", map->nb_entity);
 	i = 0;
 	while (i < (map->lx * map->ly))
@@ -148,7 +132,6 @@ int		map_load(t_game *game, t_map *map, char *path)
 	i = 0;
 	while (i < map->nb_entity)
 	{
-
 		read(fd, &tmp_x, sizeof(float));
 		read(fd, &tmp_y, sizeof(float));
 		map->entity[i].pos.x = tmp_x;
@@ -162,14 +145,21 @@ int		map_load(t_game *game, t_map *map, char *path)
 		printf("entity y = %f\n", tmp_y);
 		printf("type entity = %d\n",map->entity[i].type);
 		printf("tex entity = %d\n",map->entity[i].texture);
-
+		map->entity_ptr[i] = &map->entity[i];
 		i++;
 	}
-	weapon_load(game, map, 1);
 	return (1);
 }
 
-int		map_get_block(t_map *map, Uint8 *data, t_vect2dd pt)
+void	map_unload(t_map *map)
+{
+	free(map->floor);
+	free(map->wall);
+	free(map->ceil);
+	//map = NULL;
+}
+
+inline int		map_get_block(t_map *map, Uint8 *data, t_vect2dd pt)
 {
 	return (data[((int)pt.x) + ((int)pt.y) * map->lx]);
 }
