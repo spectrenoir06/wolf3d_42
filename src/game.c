f45e7485639f8decf2419c9b86402a404f23a343
Vallium <aalliot@student.42.fr>
2015-02-04 11:24:05 +0100

    norme drawsprite1

--- src/game.c
+++ src/game.c
@@ -246,79 +246,74 @@ inline int		sprite_compare(void *entity1, void *entity2)
 {
 	return (((t_entity *)entity2)->dist - ((t_entity *)entity1)->dist);
 }
-
+void	game_draw_sprites_init(t_drsprite *sprt, t_game *game)
+{
+	sprt->spritex = game->map.entity_ptr[sprt->x]->pos.x - game->player.pos.x;
+	sprt->spritey = game->map.entity_ptr[sprt->x]->pos.y - game->player.pos.y;
+	sprt->invdet = 1.0 / (game->player.plane.x * game->player.dir.y - game->player.dir.x * game->player.plane.y);
+	sprt->transformX = sprt->invdet * (game->player.dir.y * sprt->spritex - game->player.dir.x * sprt->spritey);
+	sprt->transformY = sprt->invdet * (-game->player.plane.y * sprt->spritex + game->player.plane.x * sprt->spritey);
+	sprt->spriteScreenX = (int)(((GAME_LX) / 2.0) * (1 + sprt->transformX / sprt->transformY));
+	sprt->spriteheight = abs((int)((GAME_LY) / sprt->transformY));
+	sprt->drawStartY = -sprt->spriteheight / 2.0 + (GAME_LY) / 2.0;
+	if (sprt->drawStartY < 0)
+		sprt->drawStartY = 0;
+	sprt->drawEndY = sprt->spriteheight / 2.0 + (GAME_LY) / 2.0;
+	if (sprt->drawEndY >= (GAME_LY))
+		sprt->drawEndY = (GAME_LY) - 1;
+	sprt->spriteWidth = abs((int)((GAME_LY) / sprt->transformY));
+	sprt->drawStartX = -sprt->spriteWidth / 2.0 + sprt->spriteScreenX;
+	if (sprt->drawStartX < 0)
+		sprt->drawStartX = 0;
+	sprt->drawEndX = sprt->spriteWidth / 2.0 + sprt->spriteScreenX;
+	if (sprt->drawEndX >= (GAME_LX))
+		sprt->drawEndX = (GAME_LX) - 1;
+	sprt->stripe = sprt->drawStartX - 1;
+}
 void	game_draw_sprites(t_game *game)
 {
-	int	y;
-	int x;
+	t_drsprite	sprt;
 
-   for(x = 0; x < game->map.nb_entity; x++)
-	  game->map.entity[x].dist = ((game->player.pos.x - game->map.entity[x].pos.x) * (game->player.pos.x - game->map.entity[x].pos.x) + (game->player.pos.y - game->map.entity[x].pos.y) * (game->player.pos.y - game->map.entity[x].pos.y));
-
+	sprt.x = -1;
+	while (++sprt.x < game->map.nb_entity)
+		game->map.entity[sprt.x].dist = ((game->player.pos.x - game->map.entity[sprt.x].pos.x) * (game->player.pos.x - game->map.entity[sprt.x].pos.x) + (game->player.pos.y - game->map.entity[sprt.x].pos.y) * (game->player.pos.y - game->map.entity[sprt.x].pos.y));
 	ft_sort_qck((void **)game->map.entity_ptr,  game->map.nb_entity, sprite_compare);
-	for(x = 0; x <  game->map.nb_entity; x++)
+	sprt.x = -1;
+	while (++sprt.x <  game->map.nb_entity)
 	{
-		double	spritex = game->map.entity_ptr[x]->pos.x - game->player.pos.x;
-		double	spritey = game->map.entity_ptr[x]->pos.y - game->player.pos.y;
 
-		double	invdet = 1.0 / (game->player.plane.x * game->player.dir.y - game->player.dir.x * game->player.plane.y);
+		game_draw_sprites_init(&sprt, game);
 
-		double	transformX = invdet * (game->player.dir.y * spritex - game->player.dir.x * spritey);
-		double	transformY = invdet * (-game->player.plane.y * spritex + game->player.plane.x * spritey);
 
-		int		spriteScreenX = (int)(((GAME_LX) / 2.0) * (1 + transformX / transformY));
-
-		int		spriteheight = abs((int)((GAME_LY) / transformY));
-
-		int		drawStartY = -spriteheight / 2.0 + (GAME_LY) / 2.0;
-		if (drawStartY < 0)
-			drawStartY = 0;
-		int		drawEndY = spriteheight / 2.0 + (GAME_LY) / 2.0;
-		if (drawEndY >= (GAME_LY))
-			drawEndY = (GAME_LY) - 1;
-
-		int		spriteWidth = abs((int)((GAME_LY) / transformY));
-		int		drawStartX = -spriteWidth / 2.0 + spriteScreenX;
-		if (drawStartX < 0)
-			drawStartX = 0;
-		int		drawEndX = spriteWidth / 2.0 + spriteScreenX;
-		if (drawEndX >= (GAME_LX))
-			drawEndX = (GAME_LX) - 1;
-
-		int stripe;
-		int	i;
-		for (stripe = drawStartX; stripe <= drawEndX; stripe++)
+		while (++sprt.stripe <= sprt.drawEndX)
 		{
-			int	texX = (int)(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * 512 / spriteWidth / 256);
+			sprt.texX = (int)(256 * (sprt.stripe - (-sprt.spriteWidth / 2 + sprt.spriteScreenX)) * 512 / sprt.spriteWidth / 256);
 
-			if (transformY > 0 && stripe > 0 && stripe < (GAME_LX) && transformY < game->Zbuffer[stripe])
+			if (sprt.transformY > 0 && sprt.stripe > 0 && sprt.stripe < (GAME_LX) && sprt.transformY < game->Zbuffer[sprt.stripe])
 			{
-				for (y = drawStartY; y < drawEndY; y++)
+				sprt.y = sprt.drawStartY - 1;
+				while (++sprt.y < sprt.drawEndY)
 				{
-					int	d = y - (GAME_LY) / 2.0 + spriteheight / 2.0;
-					int	texY = ((d * 512) / spriteheight);
-
-					t_color *color;
-					double	angle;
-					t_vect2dd	pos;
-					pos.x = game->player.pos.x - game->map.entity_ptr[x]->pos.x;
-					pos.y = game->player.pos.y - game->map.entity_ptr[x]->pos.y;
-					if (game->map.sprite[game->map.entity_ptr[x]->texture].frames == 8)
+					sprt.d = sprt.y - (GAME_LY) / 2.0 + sprt.spriteheight / 2.0;
+					sprt.texY = ((sprt.d * 512) / sprt.spriteheight);
+					sprt.pos.x = game->player.pos.x - game->map.entity_ptr[sprt.x]->pos.x;
+					sprt.pos.y = game->player.pos.y - game->map.entity_ptr[sprt.x]->pos.y;
+					if (game->map.sprite[game->map.entity_ptr[sprt.x]->texture].frames == 8)
 					{
-						angle = atan2(pos.y, pos.x) - atan2(game->map.entity_ptr[x]->dir.y, game->map.entity_ptr[x]->dir.x) + M_PI_4 / 2;
-						if (angle < 0)
-							angle += 2 * M_PI;
-						i = ((angle) / (M_PI * 2.0) * 8.0);
-						if (i > 7)
-							i = 7;
-						if (i < 0)
-							i = 0;
+						sprt.angle = atan2(sprt.pos.y, sprt.pos.x) - atan2(game->map.entity_ptr[sprt.x]->dir.y, game->map.entity_ptr[sprt.x]->dir.x) + M_PI_4 / 2;
+						if (sprt.angle < 0)
+							sprt.angle += 2 * M_PI;
+						sprt.i = ((sprt.angle) / (M_PI * 2.0) * 8.0);
+						if (sprt.i > 7)
+							sprt.i = 7;
+						if (sprt.i < 0)
+							sprt.i = 0;
 					}
 					else
-						i = 0;
-					color = (t_color *) &((Uint8*)(game->map.sprite[game->map.entity_ptr[x]->texture].tex->pixels))[(int)texX * 3 + (texY * 3 * game->map.sprite[game->map.entity_ptr[x]->texture].tex->w) + (i * 3 * TEX_SIZE)];
-					if (!(color->r == 0xFF && color->g == 0x00 && color->b == 0xFF))
-						game_draw_pixel(game, game->sdl.text_buf, GAME_X + GAME_LX - stripe, GAME_Y +  y, color);
+						sprt.i = 0;
+					sprt.color = (t_color *) &((Uint8*)(game->map.sprite[game->map.entity_ptr[sprt.x]->texture].tex->pixels))[(int)sprt.texX * 3 + (sprt.texY * 3 * game->map.sprite[game->map.entity_ptr[sprt.x]->texture].tex->w) + (sprt.i * 3 * TEX_SIZE)];
+					if (!(sprt.color->r == 0xFF && sprt.color->g == 0x00 && sprt.color->b == 0xFF))
+						game_draw_pixel(game, game->sdl.text_buf, GAME_X + GAME_LX - sprt.stripe, GAME_Y + sprt.y, sprt.color);
 				}
 			}
 		}
